0a7f3b5118b664532208dba68d5efe1a5eabc5e2
MayaSelena773 <maya.bono721@gmail.com>
2022-07-08 17:01:10 +0200

    headerdone

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <img src="../assets/img/logo-spotify.png" alt="logo" class="small-logo">
+    </header>
+</template>
+
+<script>
+export default {
+    name:'PageHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+    background-color: rgb(49, 58, 72);
+    width: 100%;
+    height: 50px;
+
+    .small-logo {
+        width: 30px;
+        margin: 0.5rem;
+    }
+}
+
+</style>
+
+
