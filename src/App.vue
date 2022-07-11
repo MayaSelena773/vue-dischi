6148f5de29c953c3c55c06a2e8bd1c151c87b136
MayaSelena773 <maya.bono721@gmail.com>
2022-07-11 17:48:22 +0200

    search

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <PageHeader />
+    <PageSearch />
     <PageMain />
   </div>
 </template>
 
 <script>
 import PageHeader from './components/PageHeader.vue';
+import PageSearch from './components/PageSearch.vue';
 import PageMain from './components/PageMain.vue';
 
 export default {
   name: 'App',
   components: {
     PageHeader,
+    PageSearch,
     PageMain
   }
 }
---
+++ src/components/PageSearch.vue
@@ -0,0 +1,51 @@
+<template>
+
+<div class="search flex">
+    
+    <input  
+    type="text" 
+    placeholder="Cerca il tuo album preferito">
+
+    <label for="contacts-search">
+        <i class="fa-solid fa-magnifying-glass my_glass"></i>
+    </label>
+
+</div>              
+    
+</template>
+
+<script>
+export default {
+    name:'PageSearch'
+}
+
+</script>
+
+<style lang="scss" scoped>
+@import './style/vars.scss';
+
+.search {
+    background-color: $brand_secondary_color;
+    padding-right: 20px;
+
+    input {
+        padding: 10px;
+        background-color: lightgreen;
+    }
+
+    .my_glass {
+        font-size: 25px;
+        padding-top: 7px;
+        padding-left: 5px;
+        color: white;
+    }
+}
+
+.flex {
+    display: flex;
+    justify-content: flex-end;
+}
+
+</style>
+
+
